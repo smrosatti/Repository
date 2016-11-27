042a6b16246e937a65208d65dfe9279c9b7cd549
TheJ0K <tiagofistarol@hotmail.com>
2016-11-27 05:17:22 -0200

    Dados necessários para fazer as medias

--- TCMDef/src/Model/Medias.java
+++ TCMDef/src/Model/Medias.java
@@ -11,34 +11,55 @@ package Model;
  */
 public class Medias {
 
+    public Medias() {
+
+    }
+
     private double agua;
     private double gasto;
     private double metrocubico;
     private int registro1;
     private int registro2;
     private int cont = 0;
-    
-    //variavel agua2 inutil?
-    //gasto é a quantidade de agua
+    private LocalDate data;
+    private int id;
 
-    public void consumoreais() {
-        setMetrocubico(1000);
+    public void consumo_de_agua() {
+        metrocubico = (1000);
 
-        setGasto(getRegistro2() - getRegistro1());
-        setGasto(getGasto() * getMetrocubico());
+        gasto = (registro2 - registro1);
+        gasto = (gasto * metrocubico);
 
-        if (getGasto() <= 10000) {
-            setAgua(33.74);
+        if (gasto <= 10000) {
+            agua = (33.74);
         } else {
-            while (getMetrocubico() * 10 < getGasto()) {
-                if (getCont() == 0) {
-                    setAgua(33.74);
+            while (metrocubico * 10 < gasto) {
+                if (cont == 0) {
+                    agua = (33.74);
                 }
-                setAgua(getAgua() + 6.84);
-                setMetrocubico(getMetrocubico() + 1000);
-                setCont(getCont() + 1);
+                agua = (agua + 6.84);
+                metrocubico = (metrocubico + 1000);
+                cont = (cont + 1);
             }
+            System.out.println(agua);
+            setAgua(agua);
         }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
     }
 
     public double getAgua() {
