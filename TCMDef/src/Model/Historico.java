ec34851710af0b1c6b1ea7f1872fc7320bf8aabe
TheJ0K <tiagofistarol@hotmail.com>
2016-11-28 15:18:16 -0200

    Update Historico.java

--- TCMDef/src/Model/Historico.java
+++ TCMDef/src/Model/Historico.java
@@ -12,46 +12,44 @@ import java.time.LocalDate;
  * @author SARA
  */
 public class Historico {
-    
-    private int id_historico;
-    private LocalDate datareg;
-    private int dadosiniciais;
-    private int dadosfinais;
-    private double media;
-    private double medialitros;
+
+    public Historico() {
+
+    }
+
+    private double agua;
+    private double gasto;
+    private double metrocubico;
+    private int registro1;
+    private int registro2;
+    private int cont = 0;
+    private LocalDate data;
+    private int id;
     private int id_media;
     private int id_usuario;
+    private int id_historico;
 
-    public LocalDate getDatareg() {
-        return datareg;
-    }
+    public void consumo_de_agua() {
+        metrocubico = (1000);
 
-    public void setDatareg(LocalDate datareg) {
-        this.datareg = datareg;
-    }
+        agua = (registro2 - registro1);
+        agua = (gasto * metrocubico);
 
-    public int getDadosiniciais() {
-        return dadosiniciais;
-    }
-
-    public void setDadosiniciais(int dadosiniciais) {
-        this.dadosiniciais = dadosiniciais;
-    }
-
-    public int getDadosfinais() {
-        return dadosfinais;
-    }
-
-    public void setDadosfinais(int dadosfinais) {
-        this.dadosfinais = dadosfinais;
-    }
-
-    public double getMedia() {
-        return media;
-    }
-
-    public void setMedia(double media) {
-        this.media = media;
+        if (agua <= 10000) {
+            gasto = (33.74);
+        } else {
+            while (metrocubico * 10 < agua) {
+                if (cont == 0) {
+                    gasto = (33.74);
+                }
+                gasto = (gasto + 6.84);
+                metrocubico = (metrocubico + 1000);
+                cont = (cont + 1);
+            }
+            System.out.println(agua);
+            setGasto(gasto);
+            setAgua(agua);
+        }
     }
 
     public int getId_historico() {
@@ -62,46 +60,83 @@ public class Historico {
         this.id_historico = id_historico;
     }
 
-    /**
-     * @return the medialitros
-     */
-    public double getMedialitros() {
-        return medialitros;
+    public double getAgua() {
+        return agua;
     }
 
-    /**
-     * @param medialitros the medialitros to set
-     */
-    public void setMedialitros(double medialitros) {
-        this.medialitros = medialitros;
+    public void setAgua(double agua) {
+        this.agua = agua;
     }
 
-    /**
-     * @return the id_media
-     */
+    public double getGasto() {
+        return gasto;
+    }
+
+    public void setGasto(double gasto) {
+        this.gasto = gasto;
+    }
+
+    public double getMetrocubico() {
+        return metrocubico;
+    }
+
+    public void setMetrocubico(double metrocubico) {
+        this.metrocubico = metrocubico;
+    }
+
+    public int getRegistro1() {
+        return registro1;
+    }
+
+    public void setRegistro1(int registro1) {
+        this.registro1 = registro1;
+    }
+
+    public int getRegistro2() {
+        return registro2;
+    }
+
+    public void setRegistro2(int registro2) {
+        this.registro2 = registro2;
+    }
+
+    public int getCont() {
+        return cont;
+    }
+
+    public void setCont(int cont) {
+        this.cont = cont;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public int getId_media() {
         return id_media;
     }
 
-    /**
-     * @param id_media the id_media to set
-     */
     public void setId_media(int id_media) {
         this.id_media = id_media;
     }
 
-    /**
-     * @return the id_usuario
-     */
     public int getId_usuario() {
         return id_usuario;
     }
 
-    /**
-     * @param id_usuario the id_usuario to set
-     */
     public void setId_usuario(int id_usuario) {
         this.id_usuario = id_usuario;
     }
-
 }
